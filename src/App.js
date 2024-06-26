5d1c5f480530ecc74efbb0c31e19850aad4b7a0f
Arasulingam <arasulingam.7639@gmail.com>
2024-06-26 15:43:19 +0530

    form updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from "react";
 import { enforceCriteria } from "./pages.js";
+
 import "./App.css";
 import axios from "axios";
 
@@ -376,11 +377,7 @@ function App() {
           {formSubmitted ? (
             <div className="testimonial-style">
               <p id="thankYouMessage">
-                <img
-                  src="https://assets-global.website-files.com/5ef0df6b9272f7410180a013/60c0e28575cd7c21701806fd_q1cunpuhbdreMPFRSFLyfUXNzpqv_I5fz_plwv6gV3sMNXwUSPrq88pC2iJijEV7wERnKXtdTA0eE4HvdnntGo9AHAWn-IcMPKV-rZw1v75vlTEoLF4OdNqsRb7C6r7Mvzrm7fe4.png"
-                  alt=""
-                  className="stars"
-                />
+                <img src="../p1.png" alt="" className="stars" />
                 <br />
                 Thanks for reaching out to us!<br></br> We'll get back to you
                 soon.
