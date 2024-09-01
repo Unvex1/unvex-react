512a30988a46b4af940d965df710e9296f235494
Arasu Lingam <87162248+Arasulingam@users.noreply.github.com>
2024-09-01 11:11:46 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -56,15 +56,7 @@ function App() {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Unvexed | Digital Marketing</title>
-        <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-GKCKMCX786"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-GKCKMCX786');
-</script>
+        
       </head>
 
       <header>
